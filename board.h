ce84305cb3f38f2db1462e1ad5911fea439e57c3
lusee88 <35087927+lusee88@users.noreply.github.com>
2018-11-19 20:43:22 -0500

    defined board.h

--- board.h
+++ board.h
@@ -1,4 +1,25 @@
 #ifndef __BOARD_H__
 #define __BOARD_H__
+#include <vector>
+#include "cell.h"
+#include "block.h"
+
+class board {
+    std::vector<std::vector<Cell>> grid;
+    int x;
+    int y;
+    Block* currblock;
+    //Xwindow window;
+
+    public:
+    void init();
+    void tick();
+    void reset();
+    bool isRowFilled(int row);
+    void clearRow(int row);
+    Cell *cellAt(int x, int y);
+
+};
+
 
 #endif
