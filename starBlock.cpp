761c909dfe4943d561f0dce09370455843333e76
owner <weihuang0313@gmail.com>
2018-12-31 01:06:13 +0800

    Deleting Unnecessary Files

--- starBlock.cpp
+++
@@ -1,9 +0,0 @@
-#include "starBlock.h"
-
-starBlock::starBlock(int x, int y, int level, Board *board): Block{x, y, level, board} {
-    blockType ='*';
-    numCells = 1;
-}
-
-void starBlock::rotateCounterClockwise() {}
-void starBlock::rotateClockwise() {}
